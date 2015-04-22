d0e4a083702f60d57801d169b38eeae27e51243a
pragyasaxena <pragyasaxena@practo.com>
2015-04-22 13:13:16 +0530

    Finished Assignment

--- cachematrix.R
+++ cachematrix.R
@@ -7,32 +7,31 @@
 ## input matrix whose inverse needs to be cached.
 
 makeCacheMatrix <- function(x = matrix()) {
-  inverse <- NULL
-  set <- function(y){
-    x<<-y
-    inverse<<-NULL
+  inverse <- NULL     #sets the inverse defined in makeCachematrix to NULL
+  set <- function(y){ #set funcn sets the value of matrix x defined in 
+    x<<-y             #makeCachematrix to y and since matrix changes, inverse
+    inverse<<-NULL    #also needs to be reset
   }
-  get <-function() x
-  setinverse<-function(inv) inverse<<-inv
-  getinverse<-function() inverse
+  get <-function() x  #returns x
+  setinverse<-function(inv) inverse<<-inv #sets inverse to the value inv
+  getinverse<-function() inverse          #returns the inverse
   list(set=set,get=get,setinverse=setinverse,getinverse=getinverse)
 }
 
 
 ## cacheSolve takes the o/p of makeCacheMatrix as input and checks if
-## inverse is already calculated or not. Then returns the inverse of the 
+## inverse is already calculated or not. Then returns the inverse of the    
 ## matrix X
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  inverse<-x$getinverse
+  inverse<-x$getinverse()            #fetches inverse in the o/p of makeCacheMatrix
   if(!is.null(inverse)){
-    message("getting cached data")
-    return(inverse)
+    message("getting cached data") #checks if inverse is not NUll
+    return(inverse)                #fetches it from cache
   }
-  data<-x$get
-  inverse<-solve(data, ...)
-  x$setinverse(inverse)
-  inverse
+  data<-x$get()                      #returns the matrix x into data
+  inverse<-solve(data, ...)        #calculates inverse
+  x$setinverse(inverse)            #sets the inverse
+  inverse                          #returns inverse of X
 }
 
